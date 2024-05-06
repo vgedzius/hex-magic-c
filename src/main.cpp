da92678d4124fbc344c2247416a26dfd087d9e7c
Vaidotas Gedzius <vaidotas@gedzius.lt>
2024-05-06 12:50:56 +0530

    Square grid

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,99 @@
 #include <SDL.h>
 
+struct Vector
+{
+    float x, y;
+};
+
+inline Vector operator*(float a, Vector b)
+{
+    Vector result;
+
+    result.x = a * b.x;
+    result.y = a * b.y;
+
+    return result;
+}
+
+inline Vector operator*(Vector b, float a)
+{
+    Vector result = a * b;
+
+    return result;
+}
+
+inline Vector &operator*=(Vector &b, float a)
+{
+    b = a * b;
+
+    return b;
+}
+
+inline Vector operator-(Vector a)
+{
+    Vector result;
+
+    result.x = -a.x;
+    result.y = -a.y;
+
+    return result;
+}
+
+inline Vector operator+(Vector a, Vector b)
+{
+    Vector result;
+
+    result.x = a.x + b.x;
+    result.y = a.y + b.y;
+
+    return result;
+}
+
+inline Vector &operator+=(Vector &a, Vector b)
+{
+    a = a + b;
+
+    return a;
+}
+
+inline Vector operator-(Vector a, Vector b)
+{
+    Vector result;
+
+    result.x = a.x - b.x;
+    result.y = a.y - b.y;
+
+    return result;
+}
+
+struct HexMetrics
+{
+    float outerRadius = 100.0f;
+    float innerRadius = outerRadius * 0.866025404f;
+
+    Vector corners[6] = {
+        {0.0f, outerRadius},
+        {innerRadius, 0.5f * outerRadius},
+        {innerRadius, -0.5f * outerRadius},
+        {0.0f, -outerRadius},
+        {-innerRadius, -0.5f * outerRadius},
+        {-innerRadius, 0.5f * outerRadius},
+    };
+};
+
+struct Cell
+{
+    Vector pos;
+};
+
+struct Grid
+{
+    int width;
+    int height;
+
+    Cell *cells;
+};
+
 int main(int, char **)
 {
     if (SDL_Init(SDL_INIT_VIDEO))
@@ -36,6 +130,23 @@ int main(int, char **)
 
     bool isRunning = true;
 
+    Grid grid;
+    grid.width = 6;
+    grid.height = 6;
+    grid.cells = (Cell *)malloc(grid.width * grid.height * sizeof(Cell));
+
+    Cell *cell = grid.cells;
+    for (int y = 0; y < grid.height; y++)
+    {
+        for (int x = 0; x < grid.width; x++)
+        {
+            cell->pos.x = x * 100.0f;
+            cell->pos.y = y * 100.0f;
+
+            cell++;
+        }
+    }
+
     while (isRunning)
     {
         SDL_Event event = {0};
@@ -59,20 +170,30 @@ int main(int, char **)
             SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
             SDL_RenderClear(renderer);
 
-            SDL_Rect rect;
+            Cell *cell = grid.cells;
+            for (int y = 0; y < grid.height; y++)
+            {
+                for (int x = 0; x < grid.width; x++)
+                {
+                    SDL_Rect rect;
 
-            rect.x = 100;
-            rect.y = 100;
-            rect.w = 50;
-            rect.h = 50;
+                    rect.x = cell->pos.x;
+                    rect.y = cell->pos.y;
+                    rect.w = 100.0f;
+                    rect.h = 100.0f;
 
-            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+                    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+                    SDL_RenderDrawRect(renderer, &rect);
 
-            SDL_RenderDrawRect(renderer, &rect);
+                    cell++;
+                }
+            }
 
             SDL_RenderPresent(renderer);
         }
     }
+
+    free(grid.cells);
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
